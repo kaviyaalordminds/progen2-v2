b6b57e2cd323c0a78043d331c25e58cf405ad8f8
muthu-dhigrowth <muthu@dhigrowth.com>
2026-05-16 11:02:29 +0530

    contact name changes

--- investor.js
+++ investor.js
@@ -46,7 +46,7 @@ const SIDEBAR_CATEGORIES = [
   { id: 'esop', label: 'ESOP', short: 'ESOP' },
   // { id: 'credit', label: 'Credit Rating', short: 'Credit' },
   { id: 'rta', label: 'Registrar and Share Transfer Agent', short: 'RTA' },
-  { id: 'investorcontact', label: 'Investor Complants Redressal', short: 'Contact' },
+  { id: 'investorcontact', label: 'Investor Complants Redressal', short: 'Investor Complants Redressal' },
   // { id: 'annual', label: 'Annual Report', short: 'Annual Report' },
   { id: 'disclosure', label: 'Disclosure - Annual Return', short: 'Disclosure' },
   { id: 'moa', label: 'Material Documents & Contracts', short: 'Material Documents & Contracts' },
